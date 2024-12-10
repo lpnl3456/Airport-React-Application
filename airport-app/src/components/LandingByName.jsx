b9d2ecfe96a07df84210edaa6f282e9272d88b2d
Samantha Thorne <150044477+13Thornes@users.noreply.github.com>
2024-12-10 10:34:10 -0330

    fixed a bug

--- airport-app/src/components/LandingByName.jsx
+++ airport-app/src/components/LandingByName.jsx
@@ -12,14 +12,15 @@ const LandingByName = ({landings})=>{
             landings.map(landingDetails => (
                     
                     <div className="landing-card" key={landingDetails.id}>
-                        <p>{landingDetails.landing.landingLocation.city.name}</p>
+                        <h3>{landingDetails.landing.landingLocation.city.name}</h3>
                         <p>{landingDetails.landing.landingTime}</p>
-                        <p>{landingDetails.landing.aircraft}</p>
+                        <p>{landingDetails.landing.aircraft.airlineName}</p>
+                        <p>{landingDetails.landing.aircraft.type}</p>
+                        <p>{landingDetails.landing.gate.gateName}</p>
                         <p>To:</p>
                         <p>{landingDetails.takeOff.takeOffLocation.city.name}</p>
                         <p>{landingDetails.takeOff.takeOffTime}</p>
-                        <p>{landingDetails.takeOff.aircraft}</p>
-                        <p>{landingDetails.takeOff.gate}</p>
+                        <p>{landingDetails.takeOff.gate.gateName}</p>
                     </div>
                 ))
             }
--- airport-app/src/components/TakeOffByName.jsx
+++ airport-app/src/components/TakeOffByName.jsx
@@ -9,14 +9,14 @@ const TakeOffByName = ({takeOffs})=>{
                 takeOffs.map(takeOffDetails => (
 
                     <div className="takeOff-card" key={takeOffDetails.flight_id}>
-                        <p>{takeOffDetails.takeOff.takeOffLocation.city.name}</p>
+                        <h3>{takeOffDetails.takeOff.takeOffLocation.city.name}</h3>
                         <p>{takeOffDetails.takeOff.takeOffTime}</p>
+                        <p>{takeOffDetails.takeOff.aircraft.airlineName}</p>
                         <p>{takeOffDetails.takeOff.aircraft.type}</p>
                         <p>{takeOffDetails.takeOff.gate.gateName}</p>
                         <p>Landing</p>
                         <p>{takeOffDetails.landing.landingLocation.city.name}</p>
                         <p>{takeOffDetails.landing.landingTime}</p>
-                        <p>{takeOffDetails.landing.aircraft.type}</p>
                         <p>{takeOffDetails.landing.gate.gateName}</p>
                     </div>
                 ))
